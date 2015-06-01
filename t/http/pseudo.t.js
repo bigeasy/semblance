50b611a2013d0224591371bcf8f795b4402831c2
Alan Gutierrez <alan@prettyrobots.com>
2015-06-01 10:24:21 -0500

    Test without `UserAgent`.
    Closes #5.

--- t/http/pseudo.t.js
+++ t/http/pseudo.t.js
@@ -1,28 +1,34 @@
 require('../proof')(1, require('cadence/redux')(prove))
 
+require('cadence/ee')
+
 function prove (async, assert) {
     var stream = require('stream')
     var Pseudo = require('../../http/pseudo'),
-        UserAgent = require('../../http/ua'),
         Binder = require('../../net/binder'),
         Bouquet = require('../../net/bouquet'),
         pems = require('../../http/pems')
-    var pseudo = new Pseudo(new Binder('https://127.0.0.1:8080', pems)),
-        ua = new UserAgent
+    var pseudo = new Pseudo(new Binder('https://127.0.0.1:8080', pems))
     var http = require('https')
-    var server = http.createServer(pems, pseudo.dispatch())
+    var server = http.createServer(pems, pseudo.dispatch()), request
     async(function () {
         server.listen(8080, '127.0.0.1', async())
     }, function () {
-        ua.fetch(pseudo.binder, {
-            url: '/test',
+        request = http.request({
+            host: '127.0.0.1',
+            port: 8080,
+            path: '/test',
+            method: 'POST',
             headers: {
                 'content-type': 'text/plain'
             },
-            payload: new Buffer(1024 * 1024 * 4)
-        }, async())
-    }, function (body, response) {
+            ca: pems.ca
+        })
+        request.end(new Buffer(1024 * 1024 * 4))
+        async.ee(request).end('response').error()
+    }, function (response) {
         assert(response.statusCode, 413, 'errored')
+        request.abort() // <- why?
     }, function () {
         server.close(async())
     })
