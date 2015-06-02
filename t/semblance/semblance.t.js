4f4065418dce27ecdb06360491fd9336f6cdd0a7
Alan Gutierrez <alan@prettyrobots.com>
2015-06-02 14:55:41 -0500

    Remove HTTPS testing.

--- t/semblance/semblance.t.js
+++ t/semblance/semblance.t.js
@@ -1,14 +1,12 @@
-require('../proof')(1, require('cadence/redux')(prove))
+require('proof')(1, require('cadence/redux')(prove))
 
 require('cadence/ee')
 
 function prove (async, assert) {
-    var stream = require('stream')
-    var Pseudo = require('../..'),
-        pems = require('../../http/pems')
+    var Pseudo = require('../..')
     var pseudo = new Pseudo
-    var http = require('https')
-    var server = http.createServer(pems, pseudo.dispatch()), request
+    var http = require('http')
+    var server = http.createServer(pseudo.dispatch()), request
     async(function () {
         server.listen(8080, '127.0.0.1', async())
     }, function () {
@@ -19,8 +17,7 @@ function prove (async, assert) {
             method: 'POST',
             headers: {
                 'content-type': 'text/plain'
-            },
-            ca: pems.ca
+            }
         })
         request.end(new Buffer(1024 * 1024 * 4))
         async.ee(request).end('response').error()
